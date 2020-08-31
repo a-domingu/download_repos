8d9b1050dc4fc3a6e79060eb28f1b357595dbe43
adomingu <alfonso.dominguez-de-la-iglesia-external@capgemini.com>
2020-08-31 10:10:31 +0200

    We changed the name of the main program

--- __init__.py
+++ __init__.py
@@ -1,3 +1,3 @@
 print('Accediendo al __init__') ### Este mensaje hay que borrarlo al final
-from download_github.download_github import main
+from download_github.download_github import download_github
 print('Ya lo hemos importado') ###Esto borrarlo también
