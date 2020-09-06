6cc4e964ca0a13dfebf31296f0569027faec7abb
Van Der Kleijn <iwan-rochus.van-der-kleijn@capgemini.com>
2020-09-06 22:29:59 +0200

    Fixed clean-up error in get_repos Test Fixture on Windows; added get_repos.cmd

--- get_repos/test_repos.py
+++ get_repos/test_repos.py
@@ -1,6 +1,26 @@
 from get_repos import parse_urls_from_text, download_repos
 import os, pytest, shutil
 
+def onerror(func, path, exc_info):
+    """
+    Error handler for ``shutil.rmtree``.
+
+    From https://stackoverflow.com/a/2656405
+
+    If the error is due to an access error (read only file)
+    it attempts to add write permission and then retries.
+
+    If the error is for another reason it re-raises the error.
+
+    Usage : ``shutil.rmtree(path, onerror=onerror)``
+    """
+    import stat
+    if not os.access(path, os.W_OK):
+        # Is the error an access error ?
+        os.chmod(path, stat.S_IWUSR)
+        func(path)
+    else:
+        raise
 
 @pytest.fixture
 def workspace():
@@ -9,7 +29,7 @@ def workspace():
         os.mkdir(path)
     yield path  
     try:
-        shutil.rmtree(path)
+        shutil.rmtree(path, onerror=onerror)
     except Exception as ex:
         print(ex)
 
